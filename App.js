c106489ace276bae89bb3bc3aa3c580056fbea82
prasadsawant7 <prasadsawant7.git@gmail.com>
2023-07-22 11:56:14 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,38 +1,191 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// React.createElement => ReactElement (JS Object) => HTMLElement (after render)
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "heading1",
-    className: "heading",
-    ["data-head"]: "Greet Message",
-    ["aria-label"]: "Greeting",
-  },
-  "Hello World from React!"
-);
-// console.log(heading); // object
+// Create a React Element Object
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Namaste React 🚀"
+// );
+
+// Create a React Root
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+/*
+ * ReactDOM takes React Element Object and converts it into an HTML Element
+ * and replaces every content which is inside that root element.
+ */
 // root.render(heading);
 
-// const child = React.createElement("div", { id: "child" }, heading);
-// const parent = React.createElement("div", { id: "parent" }, child);
-// root.render(parent);
+// JSX (It's just an Syntax) => HTML or XML like Syntax (It's not HTML or XML in JS)
+// When below line is executed then the jsxHeading will be React Element object.
+// JSX => React.createElement (Transpiled by Babel) => ReactElement (JS Object) => HTMLElement (after render)
+// const jsxHeading = (
+//   <h1 id="heading" className="head">
+//     Namasete React using JSX 🚀
+//   </h1>
+// );
+// console.log(jsxHeading);
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
-  ]),
-]);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Namaste React 🚀"
+// );
+// console.log(heading);
 
-// JSX
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(jsxHeading);
 
-console.log(parent);
-root.render(parent);
-// ReactElement(object) => HTMLElement
+// React Element
+// const header = (
+//   <span id="header" className="header">
+//     Namaste React Element
+//   </span>
+// );
+// const heading = (
+//   <h1 id="heading" className="head">
+//     {/* React Element inside React Element */}
+//     {header}
+//     Namaste React using JSX 🚀
+//   </h1>
+// );
+
+// React Component
+/*
+ * Class Based Component - Old
+ * Functional Component - New
+ */
+
+/*
+ * React Functional Component
+ * Function which returns a JSX code is a Functional Component
+ * OR
+ * A JS function which returns an React Element.
+ */
+// const HeadingComponent1 = () => {
+//   return <h1>Namaste React Functional Component 1</h1>;
+// };
+
+// const HeadingComponent2 = () => <h1>Namaste React Functional Component 2</h1>;
+
+// const HeadingComponent3 = () => (
+//   <h1 id="heading" className="heading">
+//     Namaste React Functional Component 3
+//   </h1>
+// );
+
+// const Title = () => (
+//   <h1 id="head" className="head">
+//     Namaste React using JSX 🚀
+//   </h1>
+// );
+
+// const Title1 = function () {
+//   return (
+//     <h1 id="head" className="head">
+//       Namaste React using (anonymous function) JSX 🚀
+//     </h1>
+//   );
+// };
+
+// function Title2() {
+//   return (
+//     <h1 id="head" className="head">
+//       Namaste React using (normal function) JSX 🚀
+//     </h1>
+//   );
+// }
+
+// const number = 10000;
+
+// const HeadingComponent = () => (
+//   // Component Composition => Composing 2 or more Components in one another.
+//   <div id="container">
+//     <h2>{number}</h2>
+//     <h2>{100 + 200}</h2>
+//     <h2>{console.log("Namaste React")}</h2>
+//     {/* React Element inside React Component */}
+//     {heading}
+//     <Title />
+//     <Title1 />
+//     <Title2 />
+//     <h1 className="heading">Namaste React Functional Component</h1>
+//     <HeadingComponent1 />
+//     <HeadingComponent2 />
+//     <HeadingComponent3 />
+//   </div>
+// );
+
+// const TitleElement = (
+//   <div className="titleElement">
+//     <h1>Heading Component inside Title Element</h1>
+//     {/* React Component inside React Element */}
+//     <HeadingComponent />
+//   </div>
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// // root.render(<HeadingComponent />);
+// root.render(TitleElement);
+
+const TitleComponent = () => (
+  <h1 className="head" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
+);
+
+const data = 1000;
+
+const HeadingComponent4 = () => (
+  // JSX can have only one root or parent element
+  // JSX created an empty tag
+  <>
+    {/* React Fragment - behaves like an empty tag (Provided by React) */}
+    <React.Fragment>
+      <div id="container">
+        {/* JSX sanitises the below data inside {} brackets */}
+        {data}
+        <TitleComponent />
+        <TitleComponent></TitleComponent>
+        {/* We can call any functional component inside {} brackets */}
+        {TitleComponent()}
+        <h1 className="heading">Namaste React Functional Component</h1>
+      </div>
+      <div id="container-2">
+        {/* JSX sanitises the below data inside {} brackets */}
+        {data}
+        <TitleComponent />
+        <TitleComponent></TitleComponent>
+        {/* We can call any functional component inside {} brackets */}
+        {TitleComponent()}
+        <h1 className="heading">Namaste React Functional Component</h1>
+      </div>
+    </React.Fragment>
+    <React.Fragment>
+      <div id="container">
+        {/* JSX sanitises the below data inside {} brackets */}
+        {data}
+        <TitleComponent />
+        <TitleComponent></TitleComponent>
+        {/* We can call any functional component inside {} brackets */}
+        {TitleComponent()}
+        <h1 className="heading">Namaste React Functional Component</h1>
+      </div>
+      <div id="container-2">
+        {/* JSX sanitises the below data inside {} brackets */}
+        {data}
+        <TitleComponent />
+        <TitleComponent></TitleComponent>
+        {/* We can call any functional component inside {} brackets */}
+        {TitleComponent()}
+        <h1 className="heading">Namaste React Functional Component</h1>
+      </div>
+    </React.Fragment>
+  </>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent4 />);
--- README.md
+++ README.md
@@ -1,39 +1,24 @@
-### npm doesn't stands for node package manager
+* npm start is equivalent to npm run start, because npm has already reserved a keyword for start.
+* To build the project we'll use npm run build.
 
-### package.json is a configuration for npm
+1. In JS, we have DOM Elements, just like we have React Elements in React which are equivalent to DOM Elements.
+2. While dealing with Browser's DOM API we have been provided with a separate package as ReactDOM.
+3. Core React will create an React Element object but ReactDOM will render it in the browser.
+4. JSX is not a part of React.
+5. JSX is different and React is different.
 
-- **Transitive Dependencies:** Our Project is dependent on parcel and parcel itself requires some dependencies and again those dependencies might be dependent on other dependencies this is called **Transitive Dependencies.**
+* **Transpile**: Transpiling is the process of taking source code written in one language and transforming it into another language that has a similar level of abstraction.
+* **JSX:**
+  * JS doesn't comes with JSX, even Browser's JS Engine doesn't understand JSX, JS Engine only understands ECMAScript.
+  * Even ReactDOM can't understand JSX.
+  * Even before JSX reaching to JS engine Module Bundlers like Parcel (It manages all packages one of them which is responsible for transpiling JSX is Babel) transpiles the JSX.
+  * As soon as we write code and saves the file Parcel builds the project and during that process Babel transpiles the JSX code.
+  * Then the transpiled JSX code can be understtod by React, ReactDOM and Browser's JS Engine.
+  * Babel is not created by Facebook.
+  * JSX is transpiled to React.createElement by Babel, then React.createElement conerted to an ReactElement (JS Object). Then this object is rendered as HTMLElement.
+  * JSX can have only one parent element.
+  * Babel is JS compiler, transpiler.
+  * Babel also transpiles the Newer EcmaScript to older EcmaScript to support older Browsers.
+  * If we have multiline JSX we need to wrap that code inside () round brackets to be able to understand by Babel.
 
-If you want to install, remove or upgrade a package you'll use **npm**.
-
-If you want to execute a package you'll use **npx**.
-
-Adding react and react-dom through CDN to our project is not a good way.
-
-- Fetching from a CDN will cost us network call
-- Also npm helps us to manage versions of react whereas CDN is for only fixed version.
-
-# Parcel
-
-- Dev Build
-- Local Server
-- HMR = Hot Module Replacement
-- File Watching Algorithm - written in C++
-- Caching - Faster Builds
-- Image Optimization
-- Minimization of Files
-- Compression of Files
-- Bundling
-- Consistent Hashing
-- Code Splitting
-- Differential Bundling - Support older browsers
-- Diagnostics of app
-- Error Handling
-- HTTPS
-- Tree Shaking - remove unused code
-- Different dev and prod bundles
-
-```
-npx parcel build index.html
-// This creates an error, simple remove the "main": "App.js" property, because in parcel bundling starts from index.html
-```
+> **Component Composition:** Composing 2 or more Components in one another
